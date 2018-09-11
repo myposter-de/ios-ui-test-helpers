9c5e9473ded7233f03b7dc4a26a953557c46b51f
Martin Straub <martin.straub@myposter.de>
2018-09-11 14:25:49 +0200

    Initial Commit

---
+++ UITestHelpers/UITestHelpers.h
@@ -0,0 +1,19 @@
+//
+//  UITestHelpers.h
+//  UITestHelpers
+//
+//  Created by Straub Martin on 11.09.18.
+//  Copyright © 2018 Straub Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestHelpers.
+FOUNDATION_EXPORT double UITestHelpersVersionNumber;
+
+//! Project version string for UITestHelpers.
+FOUNDATION_EXPORT const unsigned char UITestHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestHelpers/PublicHeader.h>
+
+
